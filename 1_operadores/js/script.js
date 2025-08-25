5a51b5b545d8b657532f87d49409fc342772ef06
Yuri <ndcyuri@gmail.com>
2025-08-25 11:18:16 -0300

    feat: 1_operadores

---
+++ 1_operadores/js/script.js
@@ -0,0 +1,60 @@
+// Number
+// console.log(typeof 2);
+// console.log(typeof 3.14)
+
+// // Ops. aritméticos
+// console.log(2 + 4)
+// console.log(10 - 5)
+// console.log(5 * 4)
+// console.log(10 / 2)
+
+// // special numbers
+// console.log(typeof Infinity)
+// console.log(typeof -Infinity)
+// console.log(12 * "abc")
+// console.log(typeof NaN)
+
+// // Strings
+// console.log("Hello world")
+// console.log('outro texto')
+// console.log('16')
+
+// // Caracteres especiais
+// console.log("Testando a \n quebra de linha")
+// console.log("Espaçamento de \t tab")
+
+// // Concatenação
+// console.log("oi, " + " tudo " + " bem.")
+
+// // Interpolação
+// console.log(`A soma de 2 + 2 é: ${2+2}`)
+
+// // Booleanos
+// console.log(5>20)
+// console.log(30>5)
+
+// // Comparações
+// console.log(5 <= 5)
+// console.log(5 < 5)
+// console.log(10 == 10)
+// console.log(10 == 9)
+
+// // Valor idêntico
+// console.log(9 == "9")
+
+// console.log(9 === "9")
+
+// console.log(9 != "9")
+
+// console.log(9 !== "9")
+
+// operadores lógicos
+console.log(true && true)
+console.log(true && false)
+console.log(true || true)
+console.log(true || false)
+console.log(false || false)
+console.log(!true)
+
+// empty values
+console.log(null, undefined)
