a14ece1a17aa17b1cc37b65121b00df058abcd05
Yuri <ndcyuri@gmail.com>
2025-08-26 09:45:23 -0300

    feat: 2_estruturas

---
+++ 2_estruturas/js/script.js
@@ -0,0 +1,43 @@
+// Variaveis
+// let nome = "Yuri"
+
+// console.log(nome)
+
+// nome = "Yuri Nunes"
+
+// console.log(nome)
+
+// const idade = 19
+
+// console.log(idade)
+
+// prompt
+
+// const agr = prompt("Digite sua idade:")
+
+// console.log(age)
+
+// alert
+// alert("Testando")
+
+// if
+// const m = 10
+
+// if(m > 5){
+//     console.log("M é maior que 5")
+// }
+
+// const user = "Yuri"
+
+// if(user === "Yuri"){
+//     console.log("Olá yuri")
+// }
+
+// while
+
+let p = 0
+
+while(p < 5) {
+    console.log(`Repetindo ${p}`)
+    p = p + 1;
+}
